f945591ef18f21a076069b76abe2fdf4974eb7fd
matyasosvath <matyasosv@gmail.com>
2025-03-07 13:07:52 +0100

    rename var names

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -68,34 +68,35 @@ def benchmark(document, num_bytes):
 def benchmark_test(document: str, iter: int): # permutation based
 
     num_bytes = len(str.encode(document))
-    print(f"Document num bytes: {num_bytes}")
+    print(f"document num bytes: {num_bytes}")
 
-    hutoken_results, tiktoken_results, transformers_results = [], [], []
-    count_diff_tiktoken, count_diff_transformers = 0, 0
+    count_tt, count_hf = 0, 0
+    ht_results, tt_results, hf_results = [], [], []
+
     for _ in range(iter):
-        hutoken_result, tiktoken_result, transformers_result = benchmark(document, num_bytes)
+        ht_result, tt_result, hf_result = benchmark(document, num_bytes)
 
-        hutoken_results.append(hutoken_result)
-        tiktoken_results.append(tiktoken_result)
-        transformers_results.append(transformers_result)
+        ht_results.append(ht_result)
+        tt_results.append(tt_result)
+        hf_results.append(hf_result)
 
-        if hutoken_result < mean(tiktoken_results): count_diff_tiktoken += 1
-        elif hutoken_result < mean(transformers_results): count_diff_transformers += 1
+        if ht_result < mean(tt_results): count_tt += 1
+        elif ht_result < mean(hf_results): count_hf += 1
 
     print(f"\n=== sig results ===\n")
-    print(f"Hutoken avg. result: {mean(hutoken_results)} bytes / s.")
-    print(f"Tiktoken avg. result: {mean(tiktoken_results)} bytes / s.")
-    print(f"Transformers avg. result: {mean(transformers_results)} bytes / s.")
+    print(f"hutoken avg. result: {mean(ht_results)} bytes / s.")
+    print(f"tiktoken avg. result: {mean(tt_results)} bytes / s.")
+    print(f"transformers avg. result: {mean(hf_results)} bytes / s.")
 
-    print(f"hutoken vs tiktoken: {count_diff_tiktoken / iter} p-value")
-    print(f"hutoken vs transformers: {count_diff_transformers / iter} p-value")
+    print(f"hutoken vs tiktoken: {count_tt / iter} p-value")
+    print(f"hutoken vs transformers: {count_hf / iter} p-value")
 
 
 def read_file(file_path: str) -> str:
     path = pathlib.Path(file_path)
     if not path.is_file():
-        print("Error: The file '%s' does not exist.", file_path)
-        raise RuntimeError("File not found.")
+        print("error: the file '%s' does not exist.", file_path)
+        raise RuntimeError("file not found.")
     with path.open("r", encoding="utf-8") as file:
         content = file.read()
         return content
@@ -106,9 +107,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--file-path", type=str, required=True, help="file path")
     parser.add_argument("--iter", type=int, default=1000, help="number of iterations to run the benchmark")
-    # parser.add_argument("--chunk-size", type=int, nargs="+", default=[100,1000,10000], help="list of chunk size to test tokenizer on")
+    parser.add_argument("--chunk-size", type=int, default=1000, help="chunk size to test tokenizer on")
     args = parser.parse_args()
 
     document = read_file(args.file_path)
 
-    benchmark_test(document[:100], args.iter)
+    benchmark_test(document[:args.chunk_size], args.iter)
