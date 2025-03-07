e8c2a2bb29a34e4ca7726f4c446b76f678748036
matyasosvath <matyasosv@gmail.com>
2025-03-07 12:16:24 +0100

    sig test 2

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -3,15 +3,16 @@ from typing import Any, cast
 import time
 import pathlib
 import argparse
+from statistics import mean
 
 
 __doc__ = """Measure tokenizers speed and performance for a given document."""
 
 
-def benchmark(document: str) -> None:
+def benchmark(document: str):
 
     num_bytes = len(str.encode(document))
-    print(f"num_bytes: {num_bytes}")
+    print(f"Document num bytes: {num_bytes}")
 
     import hutoken
 
@@ -21,7 +22,10 @@ def benchmark(document: str) -> None:
     start = time.perf_counter_ns()
     ht_result = hutoken.encode(document)
     end = time.perf_counter_ns()
-    print(f"hutoken \t{num_bytes / (end - start) * 1e9} bytes / s.")
+
+    ht_perf_result = num_bytes / (end - start) * 1e9
+
+    print(f"hutoken \t{ht_perf_result} bytes / s.")
 
 
     import tiktoken
@@ -32,7 +36,9 @@ def benchmark(document: str) -> None:
     start = time.perf_counter_ns()
     tt_result = enc.encode(document)
     end = time.perf_counter_ns()
-    print(f"tiktoken \t{num_bytes / (end - start) * 1e9} bytes / s.")
+
+    tt_perf_result = num_bytes / (end - start) * 1e9
+    print(f"tiktoken \t{tt_perf_result} bytes / s.")
 
 
     import transformers
@@ -44,17 +50,43 @@ def benchmark(document: str) -> None:
     start = time.perf_counter_ns()
     hf_result = hf_enc(document)
     end = time.perf_counter_ns()
-    print(f"huggingface \t{num_bytes / (end - start) * 1e9} bytes / s.")
 
-    print(f"\n=== results ===\n")
-    print(f"hutoken result: {ht_result}.\n")
-    print(f"tiktoken result: {tt_result}.\n")
-    print(f"huggingface result: {hf_result}.\n")
+    hf_perf_result = num_bytes / (end - start) * 1e9
+    print(f"huggingface \t{hf_perf_result} bytes / s.")
+
+    print(f"\n=== iter results tokens ===\n")
+    print(f"hutoken result tokens: {ht_result}.\n")
+    print(f"tiktoken result tokens: {tt_result}.\n")
+    print(f"huggingface result tokens: {hf_result}.\n")
+
+    print(f"\n=== iter results speed ===\n")
+    print(f"hutoken result: {ht_perf_result} bytes / s.")
+    print(f"tiktoken result: {tt_perf_result} bytes / s.")
+    print(f"transformers result: {hf_perf_result} bytes / s.")
+
+    return ht_perf_result, tt_perf_result, hf_perf_result
 
 
-def benchmark_significance_test(document: str) -> None: # permutation based
-    pass
+def benchmark_test(document: str, iter: int = 1000): # permutation based
+    hutoken_results, tiktoken_results, transformers_results = [], [], []
+    count_diff_tiktoken, count_diff_transformers = 0, 0
+    for _ in range(iter):
+        hutoken_result, tiktoken_result, transformers_result = benchmark(document)
 
+        hutoken_results.append(hutoken_result)
+        tiktoken_results.append(tiktoken_result)
+        transformers_results.append(transformers_result)
+
+        if hutoken_result < mean(tiktoken_results): count_diff_tiktoken += 1
+        elif hutoken_result < mean(transformers_results): count_diff_transformers += 1
+
+    print(f"\n=== sig results ===\n")
+    print(f"Hutoken avg. result: {mean(hutoken_results)} bytes / s.")
+    print(f"Tiktoken avg. result: {mean(tiktoken_results)} bytes / s.")
+    print(f"Transformers avg. result: {mean(transformers_results)} bytes / s.")
+
+    print(f"hutoken vs tiktoken: {count_diff_tiktoken / iter} p-value")
+    print(f"hutoken vs transformers: {count_diff_transformers / iter} p-value")
 
 
 def read_file(file_path: str) -> str:
@@ -71,17 +103,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='hugme cli tool')
     parser.add_argument("--file-path", type=str, required=True, help="file path")
-    parser.add_argument("--chunk-size", type=int, nargs="+", default=[100,1000,10000], help="list of chunk size to test tokenizer on")
+    parser.add_argument("--iter", type=int, default=1000, help="number of iterations to run the benchmark")
+    # parser.add_argument("--chunk-size", type=int, nargs="+", default=[100,1000,10000], help="list of chunk size to test tokenizer on")
     args = parser.parse_args()
 
-    chunk_size = args.chunk_size
     document = read_file(args.file_path)
 
-    for chunk_size in chunk_size:
-        if len(document) < chunk_size:
-            print("The document is shorter than the chunk size.")
-            continue
-        doc = document[:chunk_size]
-        print(f"chunk size: {chunk_size}")
-        print(f"document: {doc}")
-        benchmark(doc)
+    benchmark_test(document, args.iter)
