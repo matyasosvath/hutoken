2d695ccaffbf0c9cf9634a7c36844378753e9671
istvan-tamas <istvan.tamas90@gmail.com>
2025-06-25 16:16:41 +0200

    Word start and end detection using PCRE2

--- src/core.c
+++ src/core.c
@@ -118,10 +118,10 @@ void encode(char *text, struct HashMap *vocab, char *pattern, int tokens[], int 
 
     // regmatch_t match;
     
-    // char *cursor = text;
+    char *cursor = text;
 
     while(1){
-        rc = pcre2_match(re, subject, strlen((char*)subject),
+        rc = pcre2_match(re, cursor, strlen((char*)cursor),
         start_offset, 0, match_data, NULL);
 
         if (rc < 0)
@@ -142,9 +142,9 @@ void encode(char *text, struct HashMap *vocab, char *pattern, int tokens[], int 
         //while (regexec(&regex, cursor, 1, &match, 0) == 0) {
 
         //int word_start = match.rm_so;
-        int word_start = ovector[0];
+        int word_start = (int) ovector[0];
         //int word_end = match.rm_eo;
-        int word_end = ovector[1];
+        int word_end = (int) ovector[1];
 
         int word_len = word_end - word_start;
 
