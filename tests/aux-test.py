cd3674e9327909930a3c5a21da27fe0f5148726b
istvan-tamas <istvan.tamas90@gmail.com>
2025-06-30 09:41:57 +0200

    Added python style regex to pattern

--- tests/aux-test.py
+++ tests/aux-test.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer
 import hutoken
 
 hutoken.initialize("NYTK/PULI-LlumiX-32K")
-hf_enc = AutoTokenizer.from_pretrained("NYTK/PULI-LlumiX-32K")
+#hf_enc = AutoTokenizer.from_pretrained("NYTK/PULI-LlumiX-32K")
 
 print("Hutoken:\n")
 print(hutoken.encode("Gorcsev Iván, a Rangoon teherhajó matróza még huszonegy éves sem volt, midőn elnyerte a fizikai Nobel-díjat."
