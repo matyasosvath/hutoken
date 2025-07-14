e2ca9bdab3275084a16c26fabccfdc33ad4bdf67
Máté <114989158+matee8@users.noreply.github.com>
2025-07-14 12:26:13 +0000

    Fix memory leaks, undefined behavior; enhance error handling (#29)
    * fix(bpe): fix memory leak in most_common_pair.key
    
    * fix(bpe): fix zero sized VLA creation
    
    * fix(helper): add newline after debug message
    
    * fix(core): don't read unitialized data on zero-length regex match
    
    * refactor: adopt modern C practices
    
    * refactor(hash): make intent clear to static analyzers
    
    * refactor(hashmap): make casts explicit
    
    * fix(lib): use strtoul instead of sscanf for overflow checking
    
    * fix(lib): use strtol instead of atoi for overflow checking
    
    * fix(lib): use fseek instead of rewind for error checking
    
    * fix(lib): check hex format correctly
    
    * fix(lib): fix memory leak in p_initialize
    
    * fix(lib): use strtol instead of sscanf for enhanced error checking

--- src/bpe.c
+++ src/bpe.c
@@ -19,8 +19,7 @@ void create_words(char* text,
                   size_t token_num) {
     regex_t regex;
 
-    int r = regcomp(&regex, pattern, REG_EXTENDED);
-    if (r) {
+    if (regcomp(&regex, pattern, REG_EXTENDED) == true) {
         (void)fputs("Regex could not be compiled.", stderr);
         PyErr_SetString(PyExc_RuntimeError, "Regex could not be compiled.");
         return;
@@ -38,7 +37,7 @@ void create_words(char* text,
             char* start = ptr;
             char* end = ptr;
 
-            struct Boundary token_boundary = {start, end};
+            struct Boundary token_boundary = {.start = start, .end = end};
 
             token_boundaries[i] = token_boundary;
             i += 1;
@@ -55,11 +54,18 @@ void bpe_train_core(struct HashMap* vocab,
                     size_t token_num,
                     size_t vocab_size) {
     size_t token_n = token_num;
-    struct Token prev_common_pair = {"", -1};
-    struct Token most_common_pair = {"", -1};
+    struct Token prev_common_pair = {.key = NULL, .value = -1};
+    struct Token most_common_pair = {.key = NULL, .value = -1};
 
     while (vocab->count < vocab_size) {
         struct HashMap* stats = hashmap_new(token_n);
+
+        // This check prevents undefined behavior. If the number of tokens is
+        // reduced to < 2, the next loop iteration would attempt to declare
+        // a zero-sized VLA, which is illegal in C.
+        if (token_n < 2) {
+            break;
+        }
 
         // find most common pair -> next token
 
@@ -80,7 +86,7 @@ void bpe_train_core(struct HashMap* vocab,
             pair[len] = '\0';
 
             int freq = hashmap_get(stats, &(struct Token){.key = pair});
-            if (freq) {
+            if (freq != 0) {
                 hashmap_set(stats, &(struct Token){.key = strdup(pair),
                                                    .value = ++freq});
             } else {
@@ -92,6 +98,9 @@ void bpe_train_core(struct HashMap* vocab,
             int rank = hashmap_get(stats, &(struct Token){.key = pair});
 
             if (most_common_pair.value < rank) {
+                if (most_common_pair.key != NULL) {
+                    free(most_common_pair.key);
+                }
                 most_common_pair.value = rank;
                 most_common_pair.key = strdup(pair);
             }
@@ -130,8 +139,9 @@ void bpe_train_core(struct HashMap* vocab,
             strncpy(pair + l1, s2, l2);
             pair[len] = '\0';
 
-            if (strcmp(pair, most_common_pair.key) == 0) {
-                struct Boundary new_token_boundary = {s1, e2};
+            if (most_common_pair.key != NULL &&
+                strcmp(pair, most_common_pair.key) == 0) {
+                struct Boundary new_token_boundary = {.start = s1, .end = e2};
                 new_token_boundaries[j] = new_token_boundary;
                 j++;
                 i++;
@@ -151,7 +161,8 @@ void bpe_train_core(struct HashMap* vocab,
 
         hashmap_free(stats);
 
-        if (strcmp(prev_common_pair.key, most_common_pair.key) == 0) {
+        if (prev_common_pair.key != NULL &&
+            strcmp(prev_common_pair.key, most_common_pair.key) == 0) {
             break;
         }
 
@@ -159,7 +170,7 @@ void bpe_train_core(struct HashMap* vocab,
         prev_common_pair.value = most_common_pair.value;
 
         most_common_pair.value = -1;
-        most_common_pair.key = "\0";
+        most_common_pair.key = NULL;
     }
 }
 
