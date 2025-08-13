da230b277251ca0c875c44b77f45008fbfec561a
matee8 <graves-bluff-pesky@duck.com>
2025-08-13 12:54:33 +0200

    feat(scripts): add profiler script for estimating CPU cycles

---
+++ scripts/profiler.py
@@ -0,0 +1,61 @@
+"""Script to repeatedly run hutoken's encode/decode for profiling."""
+
+import argparse
+import logging
+import pathlib
+
+import hutoken
+
+logging.basicConfig(level=logging.INFO,
+                    format=('%(asctime)s - %(name)s - [%(levelname)s] - '
+                            '%(message)s'))
+
+logger = logging.getLogger(__name__)
+
+
+def read_file(path: pathlib.Path) -> str:
+    """Reads the content of the file."""
+    if not path.is_file():
+        raise FileNotFoundError(f'The file "{path}" does not exist')
+    with path.open('r', encoding='utf-8') as f:
+        return f.read()
+
+
+def main() -> None:
+    """Run the profiling workload."""
+    parser = argparse.ArgumentParser(
+        description='A script to repeatedly run hutoken\'s encode/decode for'
+        'profiling')
+
+    parser.add_argument('--file-path',
+                        type=pathlib.Path,
+                        required=True,
+                        help='path to the text file for encoding')
+    parser.add_argument('--iter',
+                        type=int,
+                        default=5000,
+                        help='number of iterations to run')
+
+    args = parser.parse_args()
+
+    logger.info('Loading document from: %s.', args.file_path)
+    document = read_file(args.file_path)
+
+    logger.debug('Initializing hutoken...')
+    hutoken.initialize(
+        './vocabs/gpt2-vocab.txt',
+        './vocabs/gpt2-vocab_special_chars.txt',
+        is_byte_encoder=True,
+    )
+    logger.debug('Initialization complete.')
+
+    logger.info('Starting profiling loop for %d iterations...', args.iter)
+    for _ in range(args.iter):
+        encoded_tokens = hutoken.encode(document)
+
+        hutoken.decode(encoded_tokens)
+    logger.info('Profiling loop finished.')
+
+
+if __name__ == '__main__':
+    main()
